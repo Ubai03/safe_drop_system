11f7fab5def756b3c15951966462d90f86984de6
Usama Ubaidillah <ubaidill4h03@gmail.com>
2026-03-18 11:20:05 +0800

    remove .env,config.php, and all related

--- database/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('BASE_PATH', __DIR__);
-$env = parse_ini_file(__DIR__ . '/.env');
-
-    foreach ($env as $key => $value) {
-        putenv("$key=$value");
-    }
--- database/login_process.php
+++ database/login_process.php
@@ -1,10 +1,5 @@
 <?php
 session_start();
-$env = parse_ini_file(__DIR__ . '/.env');
-
-foreach ($env as $key => $value) {
-    putenv("$key=$value");
-}
 
 include("to_connect.php");
 extract($_POST);
